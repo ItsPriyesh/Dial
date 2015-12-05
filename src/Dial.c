e203502da0c36b5b470faee7ab6b219b67aafa00
ItsPriyesh <priyesh.96@hotmail.com>
2015-12-04 23:26:10 -0800

    Add date

--- src/Dial.c
+++ src/Dial.c
@@ -1,12 +1,23 @@
 #include <pebble.h>
 
-static Window *s_main_window;
-static BitmapLayer *s_background_layers[4];
-static GBitmap *s_background_bitmap;
-
 #define BACKGROUND_WIDTH (1366)
 #define SCREEN_WIDTH (PBL_IF_ROUND_ELSE(180, 144))
 #define SCREEN_HEIGHT (PBL_IF_ROUND_ELSE(180, 168))
+
+static Window *s_main_window;
+static BitmapLayer *s_background_layers[4];
+static GBitmap *s_background_bitmap;
+static TextLayer *s_date_layer;
+
+static const char *days_of_week[] = {
+  "SUN",
+  "MON",
+  "TUE",
+  "WED",
+  "THU",
+  "FRI",
+  "SAT",
+};
 
 static void draw_clock(struct tm *tick_time) {
   const int64_t mins_in_day = 24 * 60;
@@ -19,11 +30,19 @@ static void draw_clock(struct tm *tick_time) {
   }
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  draw_clock(tick_time);
+static void draw_date(struct tm *tick_time) {
+  static char buffer[7];
+  snprintf(buffer, sizeof(buffer), "%s %d", days_of_week[tick_time->tm_wday], tick_time->tm_mday);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, buffer);
+  text_layer_set_text(s_date_layer, buffer);
 }
 
-static void pin_layer_update_proc(Layer *layer, GContext *ctx) {
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  draw_clock(tick_time);
+  draw_date(tick_time);
+}
+
+static void needle_layer_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorOrange);
   graphics_fill_rect(ctx, GRect(SCREEN_WIDTH / 2 - 1, 0, 2, SCREEN_HEIGHT * 0.6), 0, 0);
 }
@@ -32,8 +51,8 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  Layer *pin_layer = layer_create(bounds);
-  layer_set_update_proc(pin_layer, pin_layer_update_proc);
+  Layer *needle_layer = layer_create(bounds);
+  layer_set_update_proc(needle_layer, needle_layer_update_proc);
 
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
   for (unsigned i = 0; i < ARRAY_LENGTH(s_background_layers); i++) {
@@ -41,7 +60,17 @@ static void main_window_load(Window *window) {
     bitmap_layer_set_bitmap(s_background_layers[i], s_background_bitmap);
     layer_add_child(window_layer, (Layer*) s_background_layers[i]);
   }
-  layer_add_child(window_layer, pin_layer);
+
+  GFont date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MEDIUM_14));
+  GRect date_frame = GRect(SCREEN_WIDTH / 2 + 2, PBL_IF_ROUND_ELSE(30, 20), PBL_IF_ROUND_ELSE(67, SCREEN_WIDTH / 2 - 2), 15);
+  s_date_layer = text_layer_create(date_frame);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_font(s_date_layer, date_font);
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+
+  layer_add_child(window_layer, needle_layer);
+  layer_add_child(window_layer, (Layer*) s_date_layer);
 }
 
 static void main_window_unload(Window *window) {
@@ -49,6 +78,7 @@ static void main_window_unload(Window *window) {
     bitmap_layer_destroy(s_background_layers[i]);
   }
   gbitmap_destroy(s_background_bitmap);
+  text_layer_destroy(s_date_layer);
 }
 
 static void init() {
@@ -62,8 +92,9 @@ static void init() {
 
   time_t current_time = time(NULL);
   draw_clock(localtime(&current_time));
-
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  draw_date(localtime(&current_time));
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
